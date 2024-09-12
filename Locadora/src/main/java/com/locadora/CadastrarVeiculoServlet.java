fec144501564ff7690c33d20b6a738f42fe179c7
lucas <lucas@DESKTOP-OGLCQ4K>
2024-09-12 16:28:10 -0300

    terceiro commit

--- Locadora/src/main/java/com/locadora/CadastrarVeiculoServlet.java
+++ Locadora/src/main/java/com/locadora/CadastrarVeiculoServlet.java
@@ -17,15 +17,15 @@ public class CadastrarVeiculoServlet extends HttpServlet {
         String cor = request.getParameter("cor");
         String placa = request.getParameter("placa");
 
-        // Verifica se já existe um veículo com a mesma placa
+        
         boolean placaExistente = veiculos.stream()
                 .anyMatch(v -> v.getPlaca().equalsIgnoreCase(placa));
 
         if (placaExistente) {
-            // Se a placa já existir, redireciona para uma página de erro
+            
             response.sendRedirect("erro?mensagem=Veiculo%20com%20placa%20ja%20cadastrada");
         } else {
-            // Caso contrário, cadastra o novo veículo
+            
             Veiculo veiculo = new Veiculo(marca, modelo, ano, cor, placa);
             veiculos.add(veiculo);
             response.sendRedirect("veiculos");
